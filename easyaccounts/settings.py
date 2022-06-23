090c9ccd637d0f7dd5e67fec4a2adbab03354e37
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-06-23 15:18:46 +0500

    change static files location

--- easyaccounts/settings.py
+++ easyaccounts/settings.py
@@ -218,5 +218,5 @@ else:
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
