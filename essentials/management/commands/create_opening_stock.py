541295c4431f8661abe353c9ce8f2915676c3402
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-07-03 13:22:21 +0500

    change opening stock

--- essentials/management/commands/create_opening_stock.py
+++ essentials/management/commands/create_opening_stock.py
@@ -1,10 +1,9 @@
 import csv
 import os
 
+from authentication.models import Branch
 from django.core.management.base import BaseCommand, CommandError
 from essentials.models import Product, Stock, Warehouse
-
-RATES = {"AK": 88, "KS": 80}
 
 
 class Command(BaseCommand):
@@ -12,9 +11,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("file", type=str)
+        parser.add_argument(
+            "--delete",
+            action="store_true",
+            dest="delete",
+            default=False,
+            help="Delete all the previous opening stock",
+        )
+        parser.add_argument(
+            "-b",
+            "--branch",
+            type=str,
+            help="Branch name for deleting the opening stock for",
+        )
 
     def handle(self, *args, **options):
         file = options["file"]
+        delete = options["delete"]
+        branch = options["branch"]
         path = os.path.dirname(os.path.abspath(__file__)) + f"/data/{file}.csv"
 
         try:
@@ -22,6 +36,13 @@ class Command(BaseCommand):
                 reader = csv.reader(file, delimiter=",")
                 product = None
                 warehouse = None
+                if delete:
+                    try:
+                        branch = Branch.objects.get(name=branch)
+                        Stock.objects.filter(warehouse__branch=branch).delete()
+                    except Branch.DoesNotExist:
+                        raise CommandError(f"Branch {branch} does not exist")
+
                 for row in reader:
                     CURR_PROD = row[0]
                     CURR_WAREHOUSE = row[1]
@@ -37,12 +58,11 @@ class Command(BaseCommand):
                         if warehouse.name != CURR_WAREHOUSE:
                             warehouse = Warehouse.objects.get(name=CURR_WAREHOUSE)
 
-                    rate = RATES[product.category.name]
                     stock, created = Stock.objects.get_or_create(
                         product=product, warehouse=warehouse, yards_per_piece=row[2]
                     )
                     stock.opening_stock = row[3]
-                    stock.opening_stock_rate = rate
+                    stock.opening_stock_rate = row[4]
                     stock.save()
         except IOError:
             raise CommandError(f"{file}.csv does not exist")
