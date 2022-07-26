ba68cfc1ee70f01d1902bb12d4e99474a3e955cf
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-07-26 18:45:29 +0500

    admin models for expenses

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import *
+from .models import ExpenseAccount, ExpenseDetail
 
 admin.site.register(ExpenseAccount)
 admin.site.register(ExpenseDetail)
+
+
+class ExpenseAccountAdmin(admin.ModelAdmin):
+
+    list_display = ["name", "type"]
+    list_filter = ["name", "type"]
+
+
+class ExpenseDetailAdmin(admin.ModelAdmin):
+
+    list_display = ["date", "serial", "amount", "account_type"]
+    list_filter = ["date", "amount"]
+
+
+admin.site.register(ExpenseAccount, ExpenseAccountAdmin)
+admin.site.register(ExpenseDetail, ExpenseDetailAdmin)
