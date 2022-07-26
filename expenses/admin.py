01dd8a039e1784bda569806145d2e05ca998d072
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-07-26 20:21:18 +0500

    bug fix?

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from .models import ExpenseAccount, ExpenseDetail
 
-admin.site.register(ExpenseAccount)
-admin.site.register(ExpenseDetail)
-
 
 class ExpenseAccountAdmin(admin.ModelAdmin):
 
