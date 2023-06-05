84c9b824860aa00a007af72089befc811e2c25f6
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2023-06-05 20:00:42 +0500

    allow accountant to delete/edit transaction as well

--- transactions/views.py
+++ transactions/views.py
@@ -105,7 +105,7 @@ class GetTransaction(
 
 class EditUpdateDeleteTransaction(
     TransactionQuery,
-    IsAdminOrHeadAccountantPermissionMixin,
+    IsAdminOrAccountantOrHeadAccountantMixin,
     generics.RetrieveUpdateDestroyAPIView,
 ):
     """
