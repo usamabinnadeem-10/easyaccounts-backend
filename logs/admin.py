bc4757594bb2a000db734b2e4977cfa9bf66f2ea
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2023-09-02 21:40:05 +0500

    logs filter admin

--- logs/admin.py
+++ logs/admin.py
@@ -7,6 +7,7 @@ from .models import Log
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ["time_stamp", "type", "category", "user", "branch"]
+    list_filter = ["branch__name"]
 
 
 admin.site.register(Log, LogAdmin)
